9f3b0333ed0130e0ebdc46fb1935286d1506479c
Miguel <whytemig@gmail.com>
2024-01-03 21:31:00 -0500

    Layout

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,13 @@
+import styled, { css } from "styled-components";
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+  line-height:1.4;
+`;
+
+export default Heading;
