8d94c6b561e602f4af52977ce4e4f61bc8fee616
Miguel <whytemig@gmail.com>
2024-01-18 21:44:23 -0500

    sort/filter/pagination

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,17 +1,40 @@
+import { PAGE_SIZE } from "../ui/Pagination";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy, page }) {
+  let query = supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName, email) ");
+    .select("*, cabins(name), guests(fullName, email)", { count: "exact" });
+
+  //FILTER
+  if (filter) {
+    query = query.eq(filter.field, filter.value);
+  }
+
+  //SORTBY
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
+
+  //PAGE
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings not found");
   }
-
-  return data;
+  // console.log(data);
+  return { data, count };
 }
 
 export async function getBooking(id) {
