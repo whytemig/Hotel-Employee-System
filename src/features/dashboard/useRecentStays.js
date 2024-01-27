e9e8cc1256e1d96fe9e2e6821f460e8a24beeadf
Miguel <whytemig@gmail.com>
2024-01-27 14:33:57 -0500

    charts & timezone

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { getStaysAfterDate } from "../../services/apiBookings";
+import { subDays, format } from "date-fns";
+
+export function useRecentStays() {
+  const [searchP] = useSearchParams();
+
+  const numDays = !searchP.get("last") ? 10 : Number(searchP.get("last"));
+
+  const queryDateUTC = subDays(new Date(), numDays);
+
+  const queryDates = format(queryDateUTC, "yyyy-MM-dd HH:mm:ss.SSSxxx");
+
+  console.log(queryDates);
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDates),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  const confirmStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+
+  return { isLoading, stays, confirmStays, numDays };
+}
